6b2095b1ed9037b70e45cae7bfedfcad2033717b
Dirk <dirkkruit@live.nl>
2024-07-27 16:21:20 +0200

    Write Help content

--- src/components/HelpModal.vue
+++ src/components/HelpModal.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="help-modal" v-if="visible">
-    <h3>Help</h3>
-    <button @click="close">Close</button>
+    <h3 style="text-align: center">Help</h3>
+
+    <div>
+      Click the "Start Game" button to start playing. Use the "Reset Game" to reset the game during or after playing.
+      The objective is to fill the rows of the board with the tetrominos which fall down, Clearing rows gives points,
+      clearing multiple rows at the same time gives more points!
+
+      <h4>Controls</h4>
+      <p><span style="font-weight: bold">&#8592; :</span> Move the tetromino left.</p>
+      <p><span style="font-weight: bold">&#8594; :</span> Move the tetromino right.</p>
+      <p><span style="font-weight: bold">&#8595; :</span> Move the tetromino down.</p>
+      <p><span style="font-weight: bold">z :</span> Rotate the tetromino counter-clockwise.</p>
+      <p><span style="font-weight: bold">x :</span> Rotate the tetromino clockwise.</p>
+      <p><span style="font-weight: bold">Space:</span> Move the tetromino all the way down.</p>
+    </div>
+
+    <div style="text-align: center">
+      <button @click="close" >Close</button>
+    </div>
   </div>
 </template>
 
@@ -33,6 +50,7 @@ const close = () => {
   transform: translate(-50%, -50%);
   top: 50%;
   left: 50%;
+  text-align: left;
 }
 
 button {
